ba760eb4750b834457986584f9dd4cae5f52fae3
raphunteks <ksanihtest@gmail.com>
2026-08-18 07:52:14 +0800

    Update invoice.js

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -38,16 +38,17 @@ function addItem() {
     const tbody = document.getElementById('itemsBody');
     const index = tbody.children.length;
     
+    // Ditambahkan placeholder ekstensif dan set diskon default selalu "0" secara eksplisit
     const tr = document.createElement('tr');
     tr.innerHTML = `
         <td>
-            <input type="text" name="items[${index}][description]" class="form-control mb-1 text-sm" placeholder="Item desc" required>
-            <input type="text" name="items[${index}][period]" class="form-control text-xs" placeholder="Period (optional)">
+            <input type="text" name="items[${index}][description]" class="form-control mb-1 text-sm" placeholder="Deskripsi layanan / barang..." required>
+            <input type="text" name="items[${index}][period]" class="form-control text-xs" placeholder="Opsional (Mis: Jan 2026)">
         </td>
-        <td class="w-16"><input type="number" name="items[${index}][quantity]" class="form-control qty text-sm" value="1" min="1" oninput="calculateInvoice()"></td>
-        <td class="w-32"><input type="number" name="items[${index}][price]" class="form-control price text-sm" value="0" min="0" oninput="calculateInvoice()"></td>
-        <td class="w-20"><input type="number" name="items[${index}][discount]" class="form-control disc text-sm" value="0" min="0" oninput="calculateInvoice()"></td>
-        <td class="w-20"><input type="number" step="0.1" name="items[${index}][taxRate]" class="form-control tax text-sm" value="11" min="0" oninput="calculateInvoice()"></td>
+        <td class="w-16"><input type="number" name="items[${index}][quantity]" class="form-control qty text-sm" placeholder="1" value="1" min="1" oninput="calculateInvoice()"></td>
+        <td class="w-32"><input type="number" name="items[${index}][price]" class="form-control price text-sm" placeholder="Nominal Rp" value="0" min="0" oninput="calculateInvoice()"></td>
+        <td class="w-20"><input type="number" name="items[${index}][discount]" class="form-control disc text-sm" placeholder="0" value="0" min="0" oninput="calculateInvoice()"></td>
+        <td class="w-20"><input type="number" step="0.1" name="items[${index}][taxRate]" class="form-control tax text-sm" placeholder="11" value="11" min="0" oninput="calculateInvoice()"></td>
         <td class="w-32 text-right font-medium line-total pt-3">Rp 0</td>
         <td class="w-10 text-center pt-3"><button type="button" class="text-red-500 font-bold" onclick="this.closest('tr').remove(); calculateInvoice();">X</button></td>
     `;
